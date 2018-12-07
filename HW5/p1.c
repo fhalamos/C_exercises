85e64bab7e132b0421910615fcec33d09349c2de
Felipe Alamos <felipealamos@gmail.com>
2018-12-07 16:04:53 -0600

    Changed dijkstra to work with priority queue (minheap)

--- HW5/p1.c
+++ HW5/p1.c
@@ -20,47 +20,222 @@ typedef struct {
   Edgenode **edges;
 } Graph;
 
-void dijkstra(Graph *g, int start, int *distance){
-  int i;
-  Edgenode *p;
-  int processed[g->nvertices+1];
-  int v;
-  int w;
-  int weight;
-  int dist;
+//START HEAP STRUCTURE
 
-  /* initialize search table */
-  for (i=0; i<=g->nvertices; i++) {
-    processed[i] = FALSE;
-    distance[i] = MAXINT;
-  }
-  distance[start] = 0;
-  v = start;
+// Structure to represent a min heap node 
+typedef struct  
+{ 
+  int v; 
+  int dist; 
+} MinHeapNode; 
+
+// Structure to represent a min heap 
+typedef struct 
+{ 
+  int size;  // Number of heap nodes present currently 
+  int capacity; // Capacity of min heap 
+  int *pos;  // This is needed for decreaseKey()  // *****STILL DONT KNOW WHAT THIS IS
+  MinHeapNode **array; 
+} MinHeap; 
+
+// A utility function to create a Min Heap 
+MinHeap* createMinHeap(int capacity) 
+{ 
+  MinHeap* minHeap = (MinHeap*) malloc(sizeof(MinHeap)); 
+  minHeap->size = 0; 
+  minHeap->capacity = capacity; 
+
+  minHeap->pos = (int *)malloc(capacity * sizeof(int));
+
+  minHeap->array = 
+    (MinHeapNode**) malloc(capacity * sizeof(MinHeapNode*)); 
   
-  /* add v to processed list and update distance of neighbors */
-  while (processed[v] == FALSE) {
-    processed[v] = TRUE;
-    p = g->edges[v];    /* adjacency list for current node */
+  return minHeap; 
+} 
+
+// A utility function to create a new Min Heap Node 
+MinHeapNode* newMinHeapNode(int v, int dist) 
+{ 
+  MinHeapNode* minHeapNode =(MinHeapNode*) malloc(sizeof(MinHeapNode)); 
+  minHeapNode->v = v; 
+  minHeapNode->dist = dist; 
+  return minHeapNode; 
+}
+
+// A utility function to swap two nodes of min heap. Needed for min heapify 
+void swapMinHeapNode(MinHeapNode** a, MinHeapNode** b) 
+{ 
+  MinHeapNode* t = *a; 
+  *a = *b; 
+  *b = t; 
+}  
+
+// Function to decreasy dist value of a given vertex v. This function 
+// uses pos[] of min heap to get the current index of node in min heap 
+void decreaseKey(MinHeap* minHeap, int v, int dist) 
+{ 
+  // Get the index of v in heap array 
+  int i = minHeap->pos[v];
+
+  // Get the node and update its dist value 
+  minHeap->array[i]->dist = dist; 
+
+  // Travel up while the complete tree is not hepified. 
+  // This is a O(Logn) loop 
+  while (i && minHeap->array[i]->dist < minHeap->array[(i - 1) / 2]->dist) 
+  { 
+    // Swap this node with its parent 
+    minHeap->pos[minHeap->array[i]->v] = (i-1)/2; 
+    minHeap->pos[minHeap->array[(i-1)/2]->v] = i; 
+    swapMinHeapNode(&minHeap->array[i], &minHeap->array[(i - 1) / 2]); 
+
+    // move to parent index 
+    i = (i - 1) / 2; 
+  } 
+} 
+
+
+// A utility function to check if the given minHeap is ampty or not 
+int isEmpty(MinHeap* minHeap) 
+{ 
+  return minHeap->size == 0; 
+} 
+
+// A standard function to heapify at given idx 
+// This function also updates position of nodes when they are swapped. 
+// Position is needed for decreaseKey() 
+void minHeapify(MinHeap* minHeap, int idx) 
+{ 
+  int smallest, left, right; 
+  smallest = idx; 
+  left = 2 * idx + 1; 
+  right = 2 * idx + 2; 
+
+  if (left < minHeap->size && 
+    minHeap->array[left]->dist < minHeap->array[smallest]->dist ) 
+  smallest = left; 
+
+  if (right < minHeap->size && 
+    minHeap->array[right]->dist < minHeap->array[smallest]->dist ) 
+  smallest = right; 
+
+  if (smallest != idx) 
+  { 
+    // The nodes to be swapped in min heap 
+    MinHeapNode *smallestNode = minHeap->array[smallest]; 
+    MinHeapNode *idxNode = minHeap->array[idx]; 
+
+    // Swap positions 
+    minHeap->pos[smallestNode->v] = idx; 
+    minHeap->pos[idxNode->v] = smallest; 
+
+    // Swap nodes 
+    swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]); 
+
+    minHeapify(minHeap, smallest); 
+  } 
+} 
+
+// Standard function to extract minimum node from heap 
+MinHeapNode* extractMin(MinHeap* minHeap) 
+{ 
+  if (isEmpty(minHeap)) 
+    return NULL; 
+
+  // Store the root node 
+  MinHeapNode* root = minHeap->array[0]; 
+
+  // Replace root node with last node 
+  MinHeapNode* lastNode = minHeap->array[minHeap->size - 1]; 
+  minHeap->array[0] = lastNode; 
+
+  // Update position of last node 
+  minHeap->pos[root->v] = minHeap->size-1; 
+  minHeap->pos[lastNode->v] = 0; 
+
+  // Reduce heap size and heapify root 
+  minHeap->size--; 
+  minHeapify(minHeap, 0); 
+
+  return root; 
+} 
+
+// A utility function to check if a given vertex 
+// 'v' is in min heap or not 
+int isInMinHeap(MinHeap *minHeap, int v) 
+{ 
+if (minHeap->pos[v] < minHeap->size) 
+  return 1; 
+return 0; 
+} 
+
+//END HEAP STRUCTURE
+
+
+//distance array of with ditances to each of the nodes from start in g
+void dijkstra(Graph *g, int start, int *distance){
+  
+  //Set of all unvisited nodes in a heap
+  MinHeap* minHeap = createMinHeap(g->nvertices);
+
+
+  /* initialize search table and minheap elements */
+  for (int i=0; i<=g->nvertices; i++) {
+    
+
+    distance[i] = MAXINT;
+    minHeap->array[i] = newMinHeapNode(i, distance[i]); 
+    minHeap->pos[i] = i; 
+
+  }
+
+  // Initially size of min heap is equal to V 
+  minHeap->size = g->nvertices;
+
+
+  // Make dist value of src vertex as 0 so that it is extracted first 
+  //minHeap->array[start] = newMinHeapNode(start, distance[start]); //ESTO LOS COMENTE YO XQ NO ME HACIAN SENTIDO
+  //minHeap->pos[start] = start; 
+  distance[start] = 0; 
+  decreaseKey(minHeap, start, distance[start]); 
+
+
+  // In the followin loop, min heap contains all nodes 
+  // whose shortest distance is not yet finalized. 
+
+  Edgenode *p; // Reference to next edge of the node we are looking at, u
+  int w; // nodes adjacent to u
+  int weight; // weight of that connection
+
+  while (!isEmpty(minHeap))
+  {
+    // Extract the vertex with minimum distance value 
+    MinHeapNode* minHeapNode = extractMin(minHeap); 
+    int u = minHeapNode->v; // Store the extracted vertex number
+
+    // Traverse through all adjacent vertices of u (the extracted 
+    // vertex) and update their distance values 
+
+    p = g->edges[u]; 
     
     //Update values in table
-    while (p != NULL) {
+    while (p != NULL)
+    {
       w = p->y;
       weight = p->weight;
-      if (distance[w] > (distance[v]+weight)) 
-        distance[w] = distance[v]+weight;
+      
+
+      if(isInMinHeap(minHeap,w) && distance[w] > (distance[u]+weight)) 
+      {
+        //update distance
+        distance[w] = distance[u]+weight;
+
+        // update distance value in min heap also 
+        decreaseKey(minHeap, w, distance[w]); 
+      }
+
       p = p->next;
     }
-
-
-    /* find next closest vertex */
-    /* note: this loop is O(n), can do better */
-
-    //PENDIENTE!!! Priority queue for the next smallest node search, have to clarify that in Q1 writeup
-    for (i=0,v=1,dist=MAXINT; i<=g->nvertices; i++)
-      if ((processed[i] == FALSE) && (dist > distance[i])) {
-      	dist = distance[i];
-      	v = i; 
-      }
   }
 }
 
@@ -77,7 +252,7 @@ void dijkstra_all_src(Graph* g, int** dist)
       dijkstra(g, node_i, dist[node_i]);
 
       for (int i=0;i<g->nvertices;++i)
-        if(g->edges[i])
+        if(g->edges[i])          
           printf("d[%d]=%d\n", i,dist[node_i][i]);
       printf("\n");
     }
